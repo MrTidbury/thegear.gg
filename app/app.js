eae4fe45475c2db1da4ff318dacec69feed6f682
Jack Tidbury <mrtidbury@gmail.com>
2018-11-15 19:20:22 +0000

    Added Steam oAuth login

--- app/app.js
+++ app/app.js
@@ -3,9 +3,53 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var passport = require('passport');
+var util = require('util');
+var session = require('express-session');
+var SteamStrategy = require('passport-steam');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+var secure = require('express-force-https');
+var isDev = process.env.NODE_ENV == 'dev';
+var secrets = require('./secrets.json');
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+if (isDev){
+  passport.use(new SteamStrategy({
+    returnURL: secrets['dev']['returnURL'],
+    realm: secrets['dev']['realm'],
+    apiKey: secrets['dev']['apiKey']
+  },
+  function(identifier, profile, done) {
+    process.nextTick(function () {
+      profile.identifier = identifier;
+      return done(null, profile);
+    });
+  }
+  ));
+} else {
+  passport.use(new SteamStrategy({
+    returnURL: secrets['prod']['returnURL'],
+    realm: secrets['prod']['realm'],
+    apiKey: secrets['prod']['apiKey']
+  },
+  function(identifier, profile, done) {
+    process.nextTick(function () {
+      profile.identifier = identifier;
+      return done(null, profile);
+    });
+  }
+  ));
+}
+
 
 var app = express();
 
@@ -13,6 +57,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(session({
+  secret: 'thegearggbestreatakes',
+  name: 'logintest',
+  resave: true,
+  saveUninitialized: true}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(secure);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +73,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The Gear.gg' });
+  res.render('index', { title: 'The Gear.gg', user: req.user });
 });
+
+router.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/');
+}
 
 module.exports = router;
